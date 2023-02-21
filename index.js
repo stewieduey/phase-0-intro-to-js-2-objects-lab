23ddbbab19d1e7aa83fca030210976119c1d86ea
DuMark Allman <dumark.allman@yahoo.com>
2023-02-21 17:23:04 -0500

    [StewieDuey]

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = { Name: 'Sam', Address: '11 Broadway'};
+
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+let copy = {...object}
+copy[key] = value
+    return copy 
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+   employee[key] = value;
+   return employee; 
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+   const newEmployee = {...employee}
+   delete newEmployee[key]
+   return newEmployee
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+};
+
